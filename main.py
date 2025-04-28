0cf8fcaabe4de274e73f2ae74e463d5c984579bd
Ali <alisher.abdimuminov.2005@gmail.com>
2025-04-28 21:04:00 +0500

    bug fix

--- main.py
+++ main.py
@@ -82,9 +82,11 @@ async def stats(message: Message) -> None:
 
 @dp.message()
 async def any_message_handler(message: Message) -> None:
-    message.answer("- Murojaat yo'llash uchun @astron_corp ga yozing.")
     if message.from_user.id == int(ADMIN):
         asyncio.create_task(send_message(bot, message.text))
+    else:
+        message.answer("- Murojaat yo'llash uchun @astron_corp ga yozing.")
+
     # else:
     #     await bot.send_message(chat_id=int(ADMIN), text=f"{message.from_user.first_name} {message.from_user.last_name if message.from_user.last_name else ''}\n{'t.me/' + message.from_user.username if message.from_user.username else ''}\n\n{message.text}\n{message.from_user.id}")
 
